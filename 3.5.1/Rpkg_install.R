d5267aeef84c711d443c7003237a7c101675fd78
Mark Coggeshall <mark.coggeshall@gmail.com>
2018-10-17 11:37:41 -0400

    Install Hugo to the container's system path.

--- 3.5.1/Rpkg_install.R
+++ 3.5.1/Rpkg_install.R
@@ -3,4 +3,5 @@
 pkgs <- read.csv('rpkgs.csv', header=TRUE);
 pkgvec <- as.vector(pkgs$Package);
 install.packages(c(pkgvec));
+options(blogdown.hugo.dir = '/usr/local/bin/')
 blogdown::install_hugo()
