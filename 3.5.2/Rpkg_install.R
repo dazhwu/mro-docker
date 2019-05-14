90f76ddf4486da06bb8a19b1134955498fbf18dc
Mark Coggeshall <mark.coggeshall@gmail.com>
2019-05-14 19:06:27 -0400

    Checkpoint the image to a recent date.

--- 3.5.2/Rpkg_install.R
+++ 3.5.2/Rpkg_install.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env R
 
+checkpoint::checkpoint('2019-05-01')
 pkgs <- read.csv('rpkgs.csv', header=TRUE);
 pkgvec <- as.vector(pkgs$Package);
 install.packages(c(pkgvec));
 options(blogdown.hugo.dir = '/usr/local/bin/')
 blogdown::install_hugo()
 tinytex::install_tinytex()
-
