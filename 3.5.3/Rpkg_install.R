557d9f4e3f9f0babc8185b7bf508ba6dd5c0588b
dazhwu <63244649+dazhwu@users.noreply.github.com>
2020-06-09 16:05:40 -0400

    Small changes

--- 3.5.3/Rpkg_install.R
+++ 3.5.3/Rpkg_install.R
@@ -2,7 +2,7 @@
 
 pkgs <- read.csv('rpkgs.csv', header=TRUE);
 pkgvec <- as.vector(pkgs$Package);
-install.packages(c(pkgvec));
+install.packages(c(pkgvec), quiet = TRUE);
 options(blogdown.hugo.dir = '/usr/local/bin/')
 blogdown::install_hugo()
 tinytex::install_tinytex()
